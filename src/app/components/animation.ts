be588466c0fa865b6b8ffca84b8a38883249dde9
Javier Estevez <javierdaniloestevez@gmail.com>
2022-04-14 04:03:37 -0300

    creo animacion de fadeIn en angular para cambiar entre rutas

---
+++ src/app/components/animation.ts
@@ -0,0 +1,19 @@
+import { animate, state, style, transition, trigger } from "@angular/animations";
+
+export const fundido = 
+    trigger('fadeIn', [
+        state('*', style({
+            opacity: 1
+        })),
+        transition(':enter', [
+            style({
+                opacity: 0
+            }),
+            animate('500ms linear')
+        ]),
+        transition(':leave', [
+            animate('500ms linear', style({
+                opacity: 0
+            }))
+        ])
+    ])
