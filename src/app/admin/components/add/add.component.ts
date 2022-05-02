a776b651ffa0ef4780cbea66800f0315fba6a4af
Javier Estevez <javierdaniloestevez@gmail.com>
2022-05-02 18:34:24 -0300

    creo servicio para agregar un animal a la base de datos

--- src/app/admin/components/add/add.component.ts
+++ src/app/admin/components/add/add.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {Router, ActivatedRoute, Params} from '@angular/router';
+import { GLOBAL } from "../../../services/global";
+
+// Models
+import { Animal } from '../../../models/animal';
+
+// Servicios
+import { UserService } from "../../../services/user.service";
+import { UploadService } from '../../../services/upload.service';
+import { AnimalService } from '../../../services/animal.service';
 
 @Component({
   selector: 'admin-add',
@@ -6,9 +16,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddComponent implements OnInit {
 
-  title: string = 'Add';
+  title: string;
+  animal: Animal;
+  identity: string;
+  token: string;
+  url: string;
 
-  constructor() { }
+  constructor(
+    private _activatedRoute:ActivatedRoute, 
+    private _router: Router,
+    private _userService: UserService,
+    private _animalService: AnimalService,
+    private _uploadService: UploadService
+  ) {
+      this.title = 'Add';
+      this.animal = new Animal('', '', 2017, '', '');
+      this.identity = this._userService.getIdentity();
+      this.token = this._userService.getToken();
+      this.url = GLOBAL.url;
+   }
 
   ngOnInit(): void {
   }
---
+++ src/app/services/animal.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { GLOBAL } from './global';
+
+// Models
+import { Animal } from '../models/animal';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AnimalService {
+    public url: string;
+
+    constructor(private _http: HttpClient) {
+        this.url = GLOBAL.url; 
+    }
+
+    addAnimal(token: string, animal: Animal) {
+        // Convierto el objeto javascript a un string json
+        let params = JSON.stringify(animal);
+
+        let headers = new HttpHeaders().set('Content-Type', 'application/json')
+        .set('Authorization', token);
+
+        return this._http.post(this.url + 'animal', params, {headers: headers});                            
+    }
+}
